31b5b20bde669c927f1bf96c938f807eb015186c
eagraf <graf.ethan@gmail.com>
2018-11-21 01:33:30 -0500

    Simple framework for a moving piece on the board

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="square" style={props.style}>
+        <button className="square" onClick={props.onClick} style={props.style}>
             {props.value}
         </button>
     );
@@ -15,7 +15,7 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: [
-                [null, null, null, null, null, null, null, null],
+                ['X', null, null, null, null, null, null, null],
                 [null, null, null, null, null, null, null, null],
                 [null, null, null, null, null, null, null, null],
                 [null, null, null, null, null, null, null, null],
@@ -24,6 +24,8 @@ class Board extends React.Component {
                 [null, null, null, null, null, null, null, null],
                 [null, null, null, null, null, null, null, null],
             ],
+            selected: null,
+            moves: [],
         };
     }
 
@@ -49,16 +51,79 @@ class Board extends React.Component {
         } else {
             style = { background: 'yellow' };
         }
+        if (this.state.selected !== null) {
+            if (this.state.selected.x ===x && this.state.selected.y === y) {
+                style = { background: 'green' };
+            }
+            if (this.isAMove(x, y)) {
+                style = { background: 'red' };
+            }
+        }
 
         return ( 
             <Square 
                 value={this.state.squares[y][x]} 
                 style={style}
+                onClick={() => this.handleClick(x, y)}
             />
         ); 
     }
 
+    selectSquare(x, y) {
+        this.setState({
+            selected: {
+                x: x,
+                y: y
+            }
+        });
+        if (this.state.squares[y][x] !== null) {
+            const moves = [
+                {
+                    x: x - 1,
+                    y: y,
+                },
+                {
+                    x: x + 1,
+                    y: y,
+                },
+                {
+                    x: x,
+                    y: y-1,
+                },
+                {
+                    x: x,
+                    y: y+1,
+                },
+            ];
+            this.setState({moves});
+        } else {
+            this.setState({moves: []});
+        }
+    }
+
+    handleClick(x, y) {
+        const board = this.state.squares.slice();
+        if (this.isAMove(x, y)) {
+            board[y][x] = board[this.state.selected.y][this.state.selected.x];
+            board[this.state.selected.y][this.state.selected.x] = null;
+            this.setState({board});
+            this.selectSquare(x, y);
+        } else {
+            this.selectSquare(x, y);
+        }
+    }
+
+    isAMove(x, y) {
+        for (var i = 0; i < this.state.moves.length; i++) {
+            if (this.state.moves[i].x === x && this.state.moves[i].y === y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     render() {
+        console.log(this.state.moves);
         return (
             <div>
                 {this.renderRow(0)}
